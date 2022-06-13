2dca2bac458454dfd85d2a8934785af184b33f4a
Oskar Emilsson <oskaremilsson@gmail.com>
2022-06-13 07:57:29 +0200

    Remove use of 3d cans

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,38 @@ module.exports = {
         title: ['"EB Garamond"', 'serif'],
       },
       colors: {
+        black: '#0e100f',
         white: '#f6f5f6',
         'gagnef-green': {
-          100: '#f7fafc',
-          500: '#016c67',
-          900: '#fff000',
+          500: '#51a446',
         },
-        'gagnef-gray': '#003743',
+        'gagnef-yellow': {
+          500: '#f1c04d',
+        },
+        'gagnef-pink': {
+          500: '#f094b4',
+        },
+        'gagnef-orange': {
+          500: '#f47f54',
+        },
+        'gagnef-blue': {
+          500: '#0f70b8',
+        },
+        'gagnef-red': {
+          500: '#d81e37',
+        },
+        'gagnef-indigo': {
+          500: '#3d3b8d',
+        },
+        'gagnef-grayblue': {
+          500: '#003743',
+        },
+        'gagnef-moss': {
+          500: '#016c67',
+        },
+        'gagnef-raspberry': {
+          500: '#e01b4f',
+        }
       },
     },
   },
